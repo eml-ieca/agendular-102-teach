017e948880affe169491ce43b97f1608b920cde0
Eduardo ML (ieca) <instructor.emunozl@ieca.edu.mx>
2024-03-23 01:32:38 -0600

    env vars using

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     version: "dev",
-    apiHost: "http://localhost:3000"
+    apiHost: "http://localhost:3000/api/v1"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     version: "1.0",
-    apiHost: "http://api"
+    apiHost: "http://api.com"
 };
